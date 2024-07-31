af325b22770e3c3f0cb0bf559a774b9b4c435df4
etterath <w.rapacz@protonmail.com>
2024-07-31 14:07:39 +0200

    feat: add basic navbar

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Header from "@/components/header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -19,6 +20,7 @@ export default function RootLayout({
       <body className={`${inter.className} bg-slate-50 text-gray-950 relative h-[5000px]`}>
       <div className='bg-[#dbf7e5] absolute top-[-6rem] -z-10 right-[11rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] '></div>
         <div className='bg-[#dbd7fb] absolute top-[-1rem] -z-10 left-[35rem] h-[31.25rem] w-[31.25rem] rounded-full blur-[10rem] sm:w-[68.75rem] md:left-[-33rem] lg:left-[-28rem] xl:left-[-15rem] 2xl:left-[-5rem] '></div>
+        <Header />
         {children}
         </body>
     </html>
