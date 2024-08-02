04acdad6b1c6b863f147d7587ecb7fc060889b09
etterath <w.rapacz@protonmail.com>
2024-08-02 02:34:13 +0200

    feat: add projects section placeholders

--- components/section-divider.tsx
+++ components/section-divider.tsx
@@ -2,13 +2,20 @@
 import React from 'react';
 import { motion } from 'framer-motion';
 
-
 export default function SectionDivider() {
   return (
-    <motion.div className='bg-gray-200 my-24 h-16 w-1 rounded-full hidden sm:block'
-    initial={{opacity:0, y:100}}
-    animate={{opacity:1, y:0}}
-    transition={{delay: 0.125}}
-    ></motion.div>
+    <motion.div 
+      className='flex flex-col items-center justify-center space-y-2 my-24 hidden sm:flex'
+      initial={{opacity:0, y:100}}
+      animate={{opacity:1, y:0}}
+      transition={{delay: 0.125}}
+    >
+      {[0, 1, 2].map((index) => (
+        <div 
+          key={index} 
+          className='bg-gray-300 h-2 w-2 rounded-full'
+        />
+      ))}
+    </motion.div>
   )
 }
--- lib/data.ts
+++ lib/data.ts
@@ -2,6 +2,7 @@ import React from "react";
 import { CgWorkAlt } from "react-icons/cg";
 import { FaReact } from "react-icons/fa";
 import { LuGraduationCap } from "react-icons/lu";
+import mockImg from "@/public/skeleton.png";
 
 
 export const links = [
@@ -47,4 +48,29 @@ export const skillsData = [
   "Argilla",
   "Spark",
   "Kafka",
+] as const;
+
+
+export const projectsData = [
+  {
+    title: "Project 1",
+    description:
+      "Very funny minimalistic and eccentric placeholder for project description. It was written with being deleted later in mind. Doesn't matter tho.",
+    tags: ["React", "Next.js", "MongoDB", "Tailwind"],
+    imageUrl: mockImg,
+  },
+  {
+    title: "Project 2",
+    description:
+      "A new revolution in minimalistic and eccentric placeholders for project descriptions. It was written with being deleted later in mind. Doesn't matter tho.",
+    tags: ["React", "TypeScript", "Next.js", "Tailwind", "Redux"],
+    imageUrl: mockImg,
+  },
+  {
+    title: "Project 3",
+    description:
+      "A public responsive and minimalistic placeholder for project description. It was written with being deleted later in mind. Doesn't matter tho.",
+    tags: ["React", "Next.js", "SQL", "Tailwind", "Framer"],
+    imageUrl: mockImg,
+  },
 ] as const;
