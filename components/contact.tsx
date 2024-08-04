6f00b96617879eff9bc9eb25c103413eadb0ee0e
etterath <w.rapacz@protonmail.com>
2024-08-04 16:24:46 +0200

    feat: email annotation animation

--- components/contact.tsx
+++ components/contact.tsx
@@ -11,7 +11,23 @@ export default function Contact() {
   <section ref={ref} id="contact" className='mb-20 sm:mb-28 w-[min(100%,38rem)] text-center scroll-mt-28'>
     <SectionHeading>Contact me</SectionHeading>
     <p className='text-gray-700'>
-        Please contact me at <a className='underline' href="mailto:w.rapacz@protonmail.com">w.rapacz@protonmail.com</a> or by using code below.
+        Please contact me at <div className="group relative inline-block">
+        <a href='mailto:w.rapacz@protonmail.com' className="relative z-10 font-semibold text-gray-700">
+        w.rapacz@protonmail.com
+        </a>
+        <span className="
+            absolute 
+            bottom-0 
+            left-0 
+            w-full 
+            h-[3px] 
+            bg-green-300
+            transition-all 
+            duration-300 
+            ease-out
+            group-hover:h-full
+        "></span>
+    </div> or by using code below.
     </p>
     <div className="border border-black/[0.2] dark:border-white/[0.2] flex flex-col items-start justify-center mx-auto p-2 h-[30rem]">
     <ContactCard className='mt-10' text="contact" email='w.rapacz@protonmail.com'/>
--- components/project.tsx
+++ components/project.tsx
@@ -37,7 +37,7 @@ export default function Project({
             <p className='mt-2 leading-relaxed text-gray-700'>{description}</p>
             <ul className='flex flex-wrap mt-4 gap-2 sm:mt-auto'>
                 {tags.map((tag, index) => (
-                <li className='bg-black/[0.7] px-3 py-1 text-[0.7rem] uppercase tracking-wider text-white rounded-full' key={index}>{tag}</li>
+                <li className='bg-black/[0.5] px-3 py-1 text-[0.7rem] uppercase tracking-wider text-white rounded-full' key={index}>{tag}</li>
             ))}
         </ul>
         </div>
