763899e1586c593fe831045d237517b0257f6a1b
etterathe <w.rapacz@protonmail.com>
2024-10-26 18:56:18 +0200

    shorten about me section

--- components/about.tsx
+++ components/about.tsx
@@ -18,13 +18,10 @@ export default function About() {
     >
       <SectionHeading>About me</SectionHeading>
       <p className="mb-4">
-        I am an artificial intelligence engineer with a passion for creating
+        I am an artificial intelligence engineer that focus on creating
         practical, domain knowledge driven solutions following best MLOps
-        practices. Currently, I focus on leveraging language models and
-        knowledge graphs to enhance enterprise systems, driving efficiency in
-        business operations. My ambition is to become a T-shaped developer,
-        continuously experimenting with new ideas and technologies. I believe
-        that creating an intelligent system also requires thoughtful design.
+        practices. My ambition is to become a T-shaped developer, continuously
+        experimenting with new ideas and technologies.
       </p>
       <p>
         Beyond my professional work, I have a deep interest in exploring
